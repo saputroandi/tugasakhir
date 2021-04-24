43e74c0f52d43170000611f43682e348eb14e0c6
andi <saputro.andi593@gmail.com>
2021-04-24 19:18:45 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+    
+  <div class="p-4 bg-red-500">
+      <p class="font-semibold text-white">Hello Tailwind</p>
+  </div>
+
+</body>
+</html>
